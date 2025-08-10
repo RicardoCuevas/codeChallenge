cf3617976e6c706153ebd4cc5f254bd2dd1a3ba4
Ricardo Cuevas <rick_futbol180@hotmail.com>
2025-08-09 22:14:39 -0600

    "Update Main class to add practice convertion from Array to List and List to Arrays"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        conversionData();
+    }
+
+    public static void conversionData() {
+
+        String[] array = {"A","B","C"};
+        List<String> list = Arrays.asList(array);
+        list.stream().forEach(System.out::print);
+        System.out.println();
+        int[] intArray = {1,2,3};
+        List<Integer> intList = Arrays.stream(intArray).boxed().collect(Collectors.toList());
+        intList.stream().forEach(System.out::print);
+        System.out.println();
+        List<Integer> numberList = List.of(1,2,3,4,5,6,7);
+        Integer[] intArrays = numberList.toArray(new Integer[0]);
+        System.out.println(Arrays.toString(intArrays));
     }
 }
