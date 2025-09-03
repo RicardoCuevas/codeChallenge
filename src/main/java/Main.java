0582c2e2aabe070a0d81dcfb6f37c17f9f9ecb70
Ricardo Cuevas <rick_futbol180@hotmail.com>
2025-09-03 17:23:18 -0600

    "Update Main class remove unused import  solution"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,5 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,11 +11,11 @@ public class Main {
 
         String[] array = {"A","B","C"};
         List<String> list = Arrays.asList(array);
-        list.stream().forEach(System.out::print);
+        list.forEach(System.out::print);
         System.out.println();
         int[] intArray = {1,2,3};
-        List<Integer> intList = Arrays.stream(intArray).boxed().collect(Collectors.toList());
-        intList.stream().forEach(System.out::print);
+        List<Integer> intList = Arrays.stream(intArray).boxed().toList();
+        intList.forEach(System.out::print);
         System.out.println();
         List<Integer> numberList = List.of(1,2,3,4,5,6,7);
         Integer[] intArrays = numberList.toArray(new Integer[0]);
